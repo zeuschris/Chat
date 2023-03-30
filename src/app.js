9c898ec900f8ac0be36bb93b0328326c83d205a3
Chris Montes <zeuschris123@gmail.com>
2023-03-29 21:04:37 -0300

    App chat webscokets

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 
-app.use('/', viewsRouter)
+app.use('/views', viewsRouter)
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server on port: ${PORT}`);
